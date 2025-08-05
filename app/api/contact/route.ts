77112e51d345b5422f07fbd30d5b0d0094f1e0dc
Mohammed Sameer <rskkhan89@gmail.com>
2025-08-05 18:58:56 +0530

    update

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -25,9 +25,10 @@ export async function POST(req: Request) {
 
     // Configure transporter
     const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
+      host: 'smtp.gmail.com',
+      service: "gmail",
       port: Number(process.env.MAIL_PORT) || 465, // 587 for TLS, 465 for SSL
-      secure: false, // true for 465, false for other ports
+      secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
